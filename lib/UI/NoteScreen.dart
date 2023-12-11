1b01d5a6567e0f6e24d7eda5a819855eede42993
BonalihengOun <bonalyhengun@gmail.com>
2023-12-11 11:41:16 +0700

    New update

--- lib/UI/NoteScreen.dart
+++ lib/UI/NoteScreen.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:memo_flutter_projects/constant/colors.dart';
 import 'package:memo_flutter_projects/widget/noted_card.dart';
 
@@ -11,6 +14,14 @@ class NotedScreen extends StatefulWidget {
 }
 
 class _NotedScreenState extends State<NotedScreen> {
+  Color selectedColor = AppStyle.mainColor;
+  String _selectedType = "Not Started";
+  void changeColor(Color color) {
+    setState(() {
+      selectedColor = color;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -20,45 +31,191 @@ class _NotedScreenState extends State<NotedScreen> {
           scrollDirection: Axis.vertical,
           child: Column(
             children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: AppStyle.mainColor,
+                          ),
+                          child: IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: Image.asset(
+                              'lib/assets/image/left-arrow.png',
+                              width: 30,
+                              height: 30,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: AppStyle.mainColor,
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Image.asset(
+                              'lib/assets/image/dot.png',
+                              width: 30,
+                              height: 30,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
               Padding(
-                padding: EdgeInsets.all(25),
+                padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.06,
+                ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: AppStyle.mainColor,
-                      ),
-                      child: IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: Image.asset(
-                          'lib/assets/image/left-arrow.png',
-                          width: 30,
-                          height: 30,
+                    Row(
+                      children: [
+                        Image.asset(
+                          'lib/assets/image/Status.png',
+                          width: 14,
+                          height: 14,
                         ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Status',
+                          style: TextStyle(
+                            fontFamily: 'NiraRegular',
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          height: MediaQuery.of(context).size.height * 0.04,
+                          child: DropdownButton<String>(
+                            items: ['Not Started', 'In Progress', 'Done']
+                                .map((String item) {
+                              return DropdownMenuItem(
+                                value: item,
+                                child: Text(item),
+                              );
+                            }).toList(),
+                            value: _selectedType,
+                            onChanged: (String? newvalue) {
+                              setState(() {
+                                _selectedType = newvalue!;
+                              });
+                            },
+                            iconEnabledColor: AppStyle.mainColor,
+                            isDense: true,
+                            underline: Container(),
+                            elevation: 0,
+                            borderRadius: BorderRadius.circular(12),
+                            focusColor: Colors.amber,
+                            padding: EdgeInsets.all(8),
+                            style: TextStyle(
+                                fontFamily: 'NiraRegular',
+                                fontSize: 14,
+                                color: Colors.black.withOpacity(0.6)),
+                            hint: Text(
+                              'Not Started',
+                              style: TextStyle(
+                                  fontFamily: 'NiraRegular',
+                                  fontSize: 12,
+                                  color: Colors.black.withOpacity(0.9)),
+                            ),
+                            icon: Icon(Icons.keyboard_arrow_down),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              NoteCard(),
+              SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.only(
+                    right: MediaQuery.of(context).size.width * 0.7),
+                child: const Column(
+                  children: [
+                    Text(
+                      'Select Color',
+                      style: TextStyle(
+                          fontFamily: 'NiraSemi',
+                          fontSize: 12,
+                          color: Colors.grey),
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                        ),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('Pick a color'),
+                                content: SingleChildScrollView(
+                                  child: ColorPicker(
+                                    pickerColor: selectedColor,
+                                    onColorChanged: changeColor,
+                                    showLabel: true,
+                                    pickerAreaHeightPercent: 0.8,
+                                  ),
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('OK'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: Image.asset('lib/assets/image/colorWheel.png'),
                       ),
                     ),
+                    SizedBox(
+                      width: 10,
+                    ),
                     Container(
+                      width: 50,
+                      height: 50,
                       decoration: BoxDecoration(
+                        color: selectedColor,
                         shape: BoxShape.circle,
-                        color: AppStyle.mainColor,
-                      ),
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: Image.asset(
-                          'lib/assets/image/dot.png',
-                          width: 30,
-                          height: 30,
-                        ),
                       ),
                     ),
                   ],
                 ),
               ),
-              NoteCard(),
             ],
           ),
         ),
--- lib/widget/noted_card.dart
+++ lib/widget/noted_card.dart
@@ -2,21 +2,60 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:memo_flutter_projects/constant/colors.dart';
 
-class NoteCard extends StatelessWidget {
-  const NoteCard({super.key});
+class NoteCard extends StatefulWidget {
+  NoteCard({super.key});
 
+  @override
+  State<NoteCard> createState() => _NoteCardState();
+}
+
+class _NoteCardState extends State<NoteCard> {
+  final TextEditingController titlecontroller = TextEditingController();
+  final TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 125),
-      width: MediaQuery.of(context).size.width * 0.9,
+      width: MediaQuery.of(context).size.width * 0.88,
       height: MediaQuery.of(context).size.height * 0.4,
-      color: AppStyle.mainColor,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+      ),
       child: Column(
         children: [
           Container(
-            height: 200,
-            child: TextField(),
+            child: TextField(
+              controller: titlecontroller,
+              maxLines: 1,
+              style: TextStyle(
+                fontFamily: 'NiraBold',
+                fontSize: 30,
+              ),
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(8),
+                hintText: 'Title',
+                hintStyle: TextStyle(color: Colors.grey),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Container(
+            height: 280,
+            child: TextField(
+              controller: textEditingController,
+              maxLines: 20,
+              maxLength: 200,
+              style: TextStyle(
+                fontFamily: 'NiraSemi',
+                fontSize: 16,
+              ),
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(8),
+                helperMaxLines: 10,
+                hintText: 'Write your note. . .',
+                hintStyle: TextStyle(color: Colors.grey),
+                border: InputBorder.none,
+              ),
+            ),
           ),
         ],
       ),
